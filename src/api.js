9c139551b5aebc82fa5b62138731d23dfce6e158
atishbagate <40466303+atishbagate@users.noreply.github.com>
2021-06-28 16:19:18 +0530

    created other links. created  upcomingGamesURL, newGamesURL

--- src/api.js
+++ src/api.js
@@ -24,7 +24,12 @@ const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
 // note: use api keykey=798277c69c004edea18eb671bda9ebd2
 const popular_games = `games?key=798277c69c004edea18eb671bda9ebd2&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?key=798277c69c004edea18eb671bda9ebd2&dates=${currentDate},${nextYear}&ordering=-rating&page_size=10`;
+const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${newGames}`;
